a0ea41d8ad59e4ae84659b17d5703dd89cd670f6
Thanapat Sorralump <thanapat423@gmail.com>
2021-01-08 12:11:34 +0700

    Initial Commit

---
+++ Synthesizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Synthesizer
+//
+//  Created by Thanapat Sorralump on 8/1/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
