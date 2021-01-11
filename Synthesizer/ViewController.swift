9a96b1c3a2dc7d1f1918472351f4577b49649f0f
Thanapat Sorralump <thanapat423@gmail.com>
2021-01-11 17:14:59 +0700

    Update

--- Synthesizer/ViewController.swift
+++ Synthesizer/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
+import AVKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let synth = AVSpeechSynthesizer()
+    
+    let mockText: String = "Test Test Test Test Test Test Test Test Test Test Test Test Test Test"
+    
+    func setupCommand() {
+        MPRemoteCommandCenter.shared().playCommand.addTarget { [weak self] (event) in
+            self?.synth.continueSpeaking()
+            return .success
+        }
+        
+        MPRemoteCommandCenter.shared().pauseCommand.addTarget { [weak self] (event) in
+            self?.synth.pauseSpeaking(at: .immediate)
+            return .success
+        }
+    }
+    
+    func setupNowPlaying() {
+        var nowPlayingInfo = [String : Any]()
+        nowPlayingInfo[MPMediaItemPropertyTitle] = "Test !!!"
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+    }
+    
+    func clearNowPlaying() {
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nil
     }
 
-
+    @IBAction func speak(_ sender: UIButton) {
+        setCategory(category: .playback)
+        setupCommand()
+        let utterance = AVSpeechUtterance(string: mockText)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        utterance.volume = 1.0
+        synth.speak(utterance)
+        setupNowPlaying()
+    }
+    
+    @IBAction func stop(_ sender: UIButton) {
+        synth.stopSpeaking(at: .immediate)
+        setCategory(category: .ambient)
+        clearNowPlaying()
+    }
+    
+    private func setCategory(category: AVAudioSession.Category) {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(category)
+        } catch {
+            print(error)
+        }
+    }
 }
 
